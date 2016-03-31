6921ce393a38873b1c764aec7fa80a6a8d33c700
Marcos Pereira da Cruz <marcpcruz@gmail.com>
2016-03-31 08:12:31 -0300

    colocando mensagem de tratamento método main

--- src/freelance_recursividade_numero_harmonicos/NumeroHarmonico.java
+++ src/freelance_recursividade_numero_harmonicos/NumeroHarmonico.java
@@ -1,13 +1,26 @@
 package freelance_recursividade_numero_harmonicos;
 
+/**
+ * Execução: java NumeroHarmonico <número inteiro>
+ * 
+ * @author
+ *
+ */
 public class NumeroHarmonico {
-
-	private double inicio = 1;
 
 	public static void main(String args[]) {
 
-		double i = new NumeroHarmonico().calculaNumeroHarmonico(10000);
-		System.out.println("Numero harmonico: " + i);
+		try {
+
+			double i = new NumeroHarmonico().calculaNumeroHarmonico(new Double(
+					args[0]));
+
+			System.out.println("Numero harmonico: " + i);
+		
+		} catch (Exception e) {
+			System.out
+					.println("Erro!\nExemplo: \"java NumeroHarmonico <número inteiro>\"");
+		}
 
 	}
 
